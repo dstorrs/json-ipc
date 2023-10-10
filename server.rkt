6aba9b3b37f59db5f650711bc1b7bf81e6e5496c
dstorrs <dstorrs.public@gmail.com>
2023-10-10 11:46:45 -0400

    server.rkt: added the ability to pass a db config hash in a message

--- server.rkt
+++ server.rkt
@@ -18,11 +18,24 @@
   (close-input-port in)
   (close-output-port out))
 
+(define (send-message msg out)
+  (write-json (if (or (void?  msg)
+                      (equal? msg "\n"))
+                  "done"
+                  msg)
+              out)
+  ;;  R's readLines() function will not return until it finds a \n
+  ;;  or until the 1-minute timeout on the socket pops, the socket is
+  ;;  closed, and remaining data in the socket buffer is returned.
+  ;;  Therefore, let's send a newline.
+  (display "\n" out)
+  (flush-output out))
+
 (define (server-start dispatch)
-  (define db-path (build-path here "dbsqlite3.conf"))
+  (define db-path (build-path here "db.conf"))
   (log-server-debug "db-path is: ~a" db-path)
+
   (current-database-handle db-path)
-  (log-server-debug "outside of accept loop.  getting test rows: ~v" (query-rows (dbh) "select * from test"))
 
   (let accept-loop ()
     (define-values (in out) (tcp-accept conn))
@@ -34,27 +47,20 @@
 
       (let msg-loop ([msg-num 1])
         (displayln "waiting for message...")
-        (try [
-              (define msg (read-json in))
-              (cond [(eof-object? msg)  (close-connection in out)]
+        (try [(define msg (read-json in))
+              (match msg
+                [(? eof-object?)  (close-connection in out)]
+                ["\n" (send-message "done" out)
+                      (msg-loop (add1 msg-num))]
+                [(app (curryr dispatch msg-num) reply)
+                 #:when (not (equal? reply 'close-connection))
+                 (log-server-debug "~a got a message: ~v" (thread-id) msg)
+                 (send-message reply out)
+                 (msg-loop (add1 msg-num))]
                     [else
-                     (log-server-debug "~a msg is: ~v" (thread-id) msg)
-                     (define reply (dispatch msg msg-num))
-                     (log-server-debug "~a reply is: '~v'" (thread-id) reply)
-                     (match reply
-                       ['close-connection  (close-connection in out)]
-                       [reply
-                        (write-json (if (void? reply)
-                                        "done"
-                                        reply)
-                                    out)
-                        ;;  R's readLines() function will not return until it finds a \n
-                        ;;  or until the 1-minute timeout on the socket pops, the socket is
-                        ;;  closed, and remaining data in the socket buffer is returned.
-                        ;;  Therefore, let's send a newline.
-                        (display "\n" out)
-                        (flush-output out)
-                        (msg-loop (add1 msg-num))])])]
+                 (log-server-debug "~a closing connection" (thread-id))
+                 (send-message "FIN" out)
+                 (close-connection in out)])]
              [catch
                  (exn:break? raise)
                (any/c (let ()
@@ -63,8 +69,8 @@
                                           (if (exn? e)
                                               (exn-message e)
                                               e))
-                        (write-json "invalid message" out)
-                        (flush-output out)))]))))
+                        (send-message "invalid message" out)                        
+                        (msg-loop (add1 msg-num))))]))))
     (accept-loop)))
 
 (define (dispatch msg msg-num)
@@ -80,6 +86,9 @@
   ;;
   ;; Recognized messages:
   ;;
+  ;; * establish the DB connection parameters for this connection
+  ;; (hash 'type "db" 'action "config" 'config config-hash)
+  ;;
   ;; * run a SQL command, either a SELECT ("query"), or UPDATE/INSERT ("execute")
   ;; (hash 'type "db" 'action <"query" | "execute"> 'sql "SQL string")
   ;;       <'values (list item ...+)>?
@@ -90,21 +99,22 @@
   ;; Return value for a "query" type is always a list of lists, even if there is only one row.
   ;;
   ;; config-hash contains details on how to connect to the database, either for SQLite or
-  ;; Pg.  The string values are literals, everything else is an example:
+  ;; Pg.  Note that the sqlite version refers to a config file on the server.
   ;;
-  ;;    (hash  'connect-function   connect-func-string
+  ;;    (hash  'connect-function   "sqlite3-connect"
   ;;           'database           "./foo.db")
   ;;
-  ;; or (hash  'connect-function   connect-func-string
+  ;; or (hash  'connect-function   "postgresql-connect"
   ;;           'dbname             "lims"
   ;;           'host               "192.168.1.24"
   ;;           'port               5432
   ;;           'user               "bob"
   ;;           'password           "password123")
   ;;
-  ;; connect-func-string           <"sqlite3-connect" | "postgresql-connect">
-
   (match msg
+    [(hash-table ('type "db") ('action "config") ('config config-hash))
+     (current-database-handle config-hash)]
+    ;
     [(hash-table('type "db") ('action action) ('sql sql))
      #:when (member action '("query" "execute"))
      (log-server-debug "~a message is a valid DB message" (thread-id))
